b8e00bbdc105703cef880b8a083ed5f7b032277a
Xaver Schüll <botacode@gmail.com>
2023-04-05 13:57:36 +0200

    Re-commit with all files staged

--- pages/art-pieces/[slug].js
+++ pages/art-pieces/[slug].js
@@ -2,13 +2,9 @@ import ArtPieceDetails from "../../components/ArtPieceDetails";
 import { useRouter } from "next/router";
 
 export default function ArtPieceDetailsPage({ pieces }) {
-  console.log("pieces in ArtPieceDetailsPage():", pieces);
   const router = useRouter();
   const { slug } = router.query;
   const piece = pieces.find((piece) => piece.slug === slug);
-
-  console.log("piece in ArtPieceDetailsPage:", piece);
-  console.log("slug in ArtPieceDetailsPage:", slug);
 
   // const pieces =
 
